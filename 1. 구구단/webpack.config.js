d0aa2d56d566a982e92c8b660122d7e010d43551
dazzel3 <aakk9350@naver.com>
2022-12-19 00:23:32 +0900

    fix: fix incorrect name

--- 1. 구구단/webpack.config.js
+++ 1. 구구단/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { webpack } = require('webpack');
 
 module.exports = {
-  name: 'wordrelay-setting',
+  name: 'gugudan-setting',
   mode: 'development',
   devtool: 'eval',
   resolve: {
