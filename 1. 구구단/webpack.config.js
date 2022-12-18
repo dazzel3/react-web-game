1ae4d1cc8b2ff5ad08cf7bc87c8867dc42979ba5
dazzel3 <aakk9350@naver.com>
2022-12-18 22:41:16 +0900

    feat: add webpack setting

--- 1. 구구단/webpack.config.js
+++ 1. 구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { webpack } = require('webpack');
 
 module.exports = {
   name: 'wordrelay-setting',
@@ -18,13 +19,22 @@ module.exports = {
         test: /\.jsx?/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 5% in KR', 'last 2 chrome versions'],
+                },
+              },
+            ],
+            '@babel/preset-react',
+          ],
           plugins: ['@babel/plugin-proposal-class-properties'],
         },
       },
     ],
   },
-
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
