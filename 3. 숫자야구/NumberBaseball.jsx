fa8fe3f74d5668b48824c477c333ebb33e454c7f
dazzel3 <aakk9350@naver.com>
2022-12-23 02:34:58 +0900

    feat: refactor to functional component, apply memo

--- 3. 숫자야구/NumberBaseball.jsx
+++ 3. 숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Try from './Try';
 
 function getNumbers() {
@@ -11,48 +11,35 @@ function getNumbers() {
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(),
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers); // useState의 초기값으로 함수를 넣을 수 있다. 함수가 실행되어 return 값을 얻을 때 까지 리액트에서 기다려준다. return된 값이 state에 들어가게 된다.
+  const [tries, setTries] = useState([]);
+  const inputRef = useRef(null);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
 
-    const { value, tries, answer } = this.state;
-
     if (value === answer.join('')) {
-      this.setState((prevState) => {
-        return {
-          result: '홈런!',
-          tries: [...prevState.tries, { try: value, result: '홈런!' }],
-        };
-      });
+      setResult('홈런!');
+      setTries((prevState) => [...prevState, { try: value, result: '홈런!' }]);
       alert('게임을 다시 시작합니다!');
-      this.setState({
-        value: '',
-        answer: getNumbers(),
-        tries: [],
-      });
-      this.inputRef.current.focus();
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
+      inputRef.current.focus();
     } else {
       const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
       if (tries.length >= 9) {
-        this.setState({
-          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
-        });
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`);
         alert('게임을 다시 시작합니다!');
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: [],
-        });
-        this.inputRef.current.focus();
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
+        inputRef.current.focus();
       } else {
         for (let i = 0; i < 4; i += 1) {
           if (answerArray[i] === answer[i]) {
@@ -61,51 +48,39 @@ class NumberBaseball extends Component {
             ball += 1;
           }
         }
-        this.setState((prevState) => {
-          return {
-            tries: [
-              ...prevState.tries,
-              { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` },
-            ],
-            value: '',
-          };
-        });
-        this.inputRef.current.focus();
+        setTries((prevState) => [
+          ...prevState,
+          { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` },
+        ]);
+        setValue('');
+        inputRef.current.focus();
       }
     }
   };
 
-  onChangeInput = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  inputRef = createRef();
-
-  render() {
-    const { result, value, tries } = this.state;
-
-    return (
-      <>
-        <h1>{result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input
-            ref={this.inputRef}
-            maxLength={4}
-            value={value}
-            onChange={this.onChangeInput}
-          />
-        </form>
-        <div>시도: {tries.length}</div>
-        <ul>
-          {tries.map((v, i) => {
-            return <Try key={`${i + 1}차 시도 :`} tryInfo={v} />;
-          })}
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          ref={inputRef}
+          maxLength={4}
+          value={value}
+          onChange={onChangeInput}
+        />
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}차 시도 :`} tryInfo={v} />;
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default NumberBaseball;
--- 3. 숫자야구/Try.jsx
+++ 3. 숫자야구/Try.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-class Try extends Component {
-  render() {
-    const { tryInfo } = this.props;
-    return (
-      <li>
-        <div>{tryInfo.try}</div>
-        <div>{tryInfo.result}</div>
-      </li>
-    );
-  }
-}
+// PureComponent의 역할은 두 가지가 있다. 1. state의 값이 변경될 때만 리렌더링 해주기, 2.props 값이 변경될 때만 리렌더링 해주기
+// React.memo는 class형 컴포넌트에서의 PureComponent의 역할 중 props 값이 변경될 때만 리렌더링 해주는 역할과 같다.
+// 부모 컴포넌트가 리렌더링 되면 자식 컴포넌트도 같이 리렌더링 되는데 이를 방지하고, props 값이 변경될 때만 리렌더링 되도록 해준다.
+
+const Try = memo(({ tryInfo }) => {
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div>{tryInfo.result}</div>
+    </li>
+  );
+});
 
 export default Try;
